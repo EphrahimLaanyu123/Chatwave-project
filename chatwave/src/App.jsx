41484428e9cda4c00f3a57a2c6360dea068635cf
user <ephrahim.lenaiayasa@student.moringaschool.com>
2023-10-05 18:59:41 +0300

    Committing local changes in App.jsx

--- chatwave/src/App.jsx
+++ chatwave/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginForm from "./LoginForm";
 import Homepage from "./Home";
-import Main from "./Main";
+// import Main from "./Main";
 import { io } from 'socket.io-client'
 
 const socket = io('http://localhost:5000/')
@@ -15,17 +15,16 @@ function App() {
 
   return (
     <div>
-    {/* <Router> */}
-      {/* <Routes> */}
-        {/* <Route path="/login" element={<LoginForm/>} /> */}
-        {/* <Route path="/" element={<Homepage/>} /> */}
-        {/* Add more routes as needed */}
-      {/* </Routes> */}
-    {/* </Router> */}
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginForm/>} />
+        <Route path="/" element={<Homepage/>} />
+      </Routes>
+    </Router>
 
-    <Main socket={socket}/>
-    </div>
+    {/* <Main socket={socket}/> */}
+    </div> 
   );
-}
+  }
 
 export default App;
