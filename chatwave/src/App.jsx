6bc21d0f44c3ec256ade24da206ecac73b9a7b25
gilbert-ku <gilbert45ku@gmail.com>
2023-10-08 22:50:09 +0300

    add login and chat room

--- chatwave/src/App.jsx
+++ chatwave/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import "./App.css"; 
+import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginForm from "./LoginForm";
-import SignupForm from "./SignupForm"; 
+import SignupForm from "./SignupForm";
 import Homepage from "./Home";
-// import AuthForm from "./AuthForm";
+import Chatroom from "./ChatroomPage";
+import Friends from "./friends";
 
 function App() {
   return (
@@ -13,8 +14,8 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/signup" element={<SignupForm />} />
-        {/* <Route path="/authform" element={<AuthForm />} /> */}
-        
+        <Route path="/chatroom" element={<Chatroom />} />
+        <Route path="/friends" element={<Friends />} /> 
       </Routes>
     </Router>
   );
--- chatwave/src/ChatroomPage.jsx
+++ chatwave/src/ChatroomPage.jsx
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
 
+
+
+// Chatroom component
 function Chatroom() {
   const [users, setUsers] = useState([]);
+  const [friends, setFriends] = useState([]);
 
   useEffect(() => {
     // Fetch the list of users when the chatroom component mounts
-    axios.get("http://127.0.0.1:5000/chatroom/users")
+    axios.get("http://127.0.0.1:5000/users")
       .then((response) => {
         // Update the state with the list of users
         setUsers(response.data);
@@ -16,18 +18,39 @@ function Chatroom() {
       });
   }, []);
 
+  const handleAddFriend = (userId) => {
+    // Assuming you have a function to add a friend to the list
+    // You can modify this logic based on your actual implementation
+    // For now, let's assume you have a setFriends function to update the friends list
+    setFriends((prevFriends) => [...prevFriends, users.find(user => user.id === userId)]);
+  };
+
   return (
-    <div>
-      <h2>Chatroom</h2>
+    <div className="bg-blue-500 p-4">
+      <nav className="flex justify-between items-center mb-4">
+        <div className="text-white text-2xl font-bold">Chatwave</div>
+        <div className="flex space-x-4">
+          
+        </div>
+      </nav>
+
       <div>
-        <h3>Users in the Chatroom:</h3>
-        <p>
+        <h2 className="text-white text-3xl mb-4">Chatroom</h2>
+        <div>
+          <h3 className="text-white text-xl mb-2">Users in the Chatroom:</h3>
           {users.map((user) => (
-            <span key={user.id}>{user.username}, </span>
+            <div key={user.id} className="bg-white p-4 mb-2 flex justify-between items-center">
+              <span>{user.username}</span>
+              <button
+                onClick={() => handleAddFriend(user.id)}
+                className="bg-blue-500 text-white px-2 py-1 rounded"
+              >
+                Add Friend
+              </button>
+            </div>
           ))}
-        </p>
+        </div>
       </div>
-      {/* Add your chatroom functionality here */}
     </div>
   );
 }
